5f3ffa94f5cf8066e34c26e9b00825a602f1c198
gg <1215rkddn@naver.com>
2020-02-13 16:19:22 +0900

    sdf

--- BlackMoney.java
+++ BlackMoney.java
@@ -2,6 +2,6 @@ public class BlackMoney{
  public static void main(String[]args){
   System.out.println("Black Money");
 
-  System.out,pirntln("Testing.... );
+  System.out,pirntln("Test );
    }
 }
